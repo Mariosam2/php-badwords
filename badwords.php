1cfe8198b50d06554e97aa1c3077456ca81320d6
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-25 14:24:53 +0100

    added case insensitive logic

--- badwords.php
+++ badwords.php
@@ -2,10 +2,10 @@
 $text = $_GET["paragraph"];
 $badWord = $_GET["badword"];
 
-$censored_text = str_replace($badWord, '***', $text);
+$censored_text = str_replace(strtolower($badWord), '***', strtolower($text));
 ?>
 
 
 <h1><?php echo $text . ' ' . strlen($text) ?></h1>
 
-<h1><?php echo $censored_text . ' ' . strlen($censored_text) ?></h1>
+<h1><?php echo  ucfirst($censored_text) . ' ' . strlen($censored_text) ?></h1>
